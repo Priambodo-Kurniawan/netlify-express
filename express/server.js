0ee68481a62af8190b9217a993f5a11f2e029a5e
Priambodo-Kurniawan <priambodo.2009@gmail.com>
2019-08-13 18:41:26 +0700

    fetch api google sheet

--- express/server.js
+++ express/server.js
@@ -3,6 +3,13 @@ const express = require('express');
 const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
+const cors = require('cors');
+const axios = require('axios');
+
+let url =
+  'https://spreadsheets.google.com/feeds/list/1J3qw1ONH-Om2qfjSemzeAdLutZmuC_D1LRynm_f35Vs/od6/public/basic?alt=json';
+
+app.use(cors());
 
 const router = express.Router();
 router.get('/', (req, res) => {
@@ -13,8 +20,35 @@ router.get('/', (req, res) => {
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
 
+router.get('/list-service', (req, res) => {
+  axios
+    .get(url)
+    .then(response => {
+      let sheets = response.data.feed.entry;
+      if (sheets.length) {
+        let arr = [];
+        sheets.forEach(data => {
+          let obj = {};
+          obj.data = data.content.$t.split(', column').forEach((d, idx) => {
+            let temp = d.split(': ');
+            if (idx == 0) {
+              obj[temp[0].split('column')[1]] = temp[1];
+            } else {
+              obj[temp[0]] = temp[1];
+            }
+          });
+          arr.push(obj);
+        });
+        res.json(arr);
+      }
+    })
+    .catch(err => {
+      res.send(err);
+    });
+});
+
 app.use(bodyParser.json());
-app.use('/.netlify/functions/server', router);  // path must route to lambda
+app.use('/.netlify/functions/server', router); // path must route to lambda
 
 module.exports = app;
 module.exports.handler = serverless(app);
