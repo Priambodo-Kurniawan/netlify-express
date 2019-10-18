0cddac3cf9a7d2b99aa4d5f0fbda18a503e291d3
Priambodo-Kurniawan <priambodo.2009@gmail.com>
2019-10-18 20:35:17 +0700

    add function post

--- express/server.js
+++ express/server.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const GoogleSpreadsheet = require('google-spreadsheet');
 const { promisify } = require('util');
+const { check, validationResult } = require('express-validator');
 require('dotenv').config();
 
 const creds = require('../client_secret.json');
@@ -23,6 +24,13 @@ async function accessSpreadsheet(callback) {
 }
 
 app.use(cors());
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
 
 const router = express.Router();
 router.get('/', (req, res) => {
@@ -32,6 +40,39 @@ router.get('/', (req, res) => {
 });
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
+
+// save to spreadsheet
+router.post('/submit-email', [check('email').isEmail()], (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
+
+  // Create a document object using the ID of the spreadsheet - obtained from its URL.
+  var doc = new GoogleSpreadsheet(
+    '1AdTgYxntJZ0CSiSWiaD94NPFNatahd0ms3c5UO58cso'
+  );
+
+  // Authenticate with the Google Spreadsheets API.
+  doc.useServiceAccountAuth(creds, function(err) {
+    // Get all of the rows from the spreadsheet.
+    let timestamp = new Date();
+    let answer = Math.ceil(Math.random() * 4);
+    doc.addRow(1, { email: req.body.email, timestamp: timestamp }, function(
+      err
+    ) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send({
+          status: 200,
+          message: 'success',
+          answer: answer
+        });
+      }
+    });
+  });
+});
 
 router.get('/list-service', (req, res) => {
   accessSpreadsheet(function(hai) {
